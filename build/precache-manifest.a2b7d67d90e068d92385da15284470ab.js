e729bab863abc4f09e706bf19b25256fc1f0b9d7
takeshwar <takeshwarverma124@gmail.com>
2023-12-19 23:45:43 +0530

    create small server for run build file

---
+++ build/precache-manifest.a2b7d67d90e068d92385da15284470ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84b115e2b1da97f259bf",
+    "url": "/static/css/main.0f6fddf4.chunk.css"
+  },
+  {
+    "revision": "84b115e2b1da97f259bf",
+    "url": "/static/js/main.40d2476c.chunk.js"
+  },
+  {
+    "revision": "6a81064e3b4189a31fd1",
+    "url": "/static/js/runtime~main.e6002803.js"
+  },
+  {
+    "revision": "6ab6d5b9b4c4f99daa16",
+    "url": "/static/js/2.28824e2e.chunk.js"
+  },
+  {
+    "revision": "d06d8fe717dc215d2ddf",
+    "url": "/static/js/3.71e6714e.chunk.js"
+  },
+  {
+    "revision": "3ec22cd9df4c63f9e8e52ff4a4b793f7",
+    "url": "/index.html"
+  }
+];
